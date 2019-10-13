f1a0eb8b607677657e9985ce0c88fa2a18419618
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-10-13 20:24:36 +0100

    fill in todos

--- scrollytelling/config.js
+++ scrollytelling/config.js
@@ -1,9 +1,9 @@
 var config = {
-    style: 'YOUR_STYLE_URL', //TODO
-    accessToken: 'YOUR_ACCESS_TOKEN', //TODO
-    title: 'YOUR MAP TITLE', //TODO
-    subtitle: 'YOUR MAP SUBTITLE', //TODO
-    byline: 'By YOUR NAME', //TODO
+    style: 'mapbox://styles/troy34/ck1j68ua82mtu1cnp55bmatuk', //TODO
+    accessToken: 'pk.eyJ1IjoidHJveTM0IiwiYSI6ImNqa2w1c2FoYzA1MGYza285aGx0cHVjNWIifQ.YTg83cMNLzog9aysZKZYjQ', //TODO
+    title: 'My Home Map', //TODO
+    subtitle: 'Places I have lived.', //TODO
+    byline: 'By Chuks Opia', //TODO
     footer: 'Created at Concatenate Lagos 2019',
     showMarkers: false,
     theme: 'light',
@@ -11,13 +11,97 @@ var config = {
     chapters: [
         //TODO replace this data with data from the first point in your dataset
         {
-            id: 'concatenate-lagos',
-            title: 'Concatenate Lagos',
-            description: 'A free conference for African developers with speakers from around the world.',
+            id: 'ilasa-lagos',
+            title: 'Ilasamaja, Lagos',
+            description: 'First place I lived as a kid',
             location: {
               "center": [
-                3.366639462826271,
-                6.51635441424402
+                3.32173,
+                6.529123
+              ],
+              "zoom": 11.805394255186213,
+              "pitch": 33.499999999999986,
+              "bearing": 136.79999999999995
+            }
+        },
+        {
+            id: 'idimu-lagos',
+            title: 'Idimu, Lagos',
+            description: 'Second place I lived as a kid',
+            location: {
+              "center": [
+                3.283847,
+                6.57522
+              ],
+              "zoom": 11.805394255186213,
+              "pitch": 33.499999999999986,
+              "bearing": 136.79999999999995
+            }
+        },
+        {
+            id: 'ikotun-lagos',
+            title: 'Ikotun, Lagos',
+            description: 'Third place I lived as a kid',
+            location: {
+              "center": [
+                3.265356,
+                6.543339
+              ],
+              "zoom": 11.805394255186213,
+              "pitch": 33.499999999999986,
+              "bearing": 136.79999999999995
+            }
+        },
+        {
+            id: 'mowe-ogun',
+            title: 'Mowe, Ogun',
+            description: 'Fourth place I lived as a kid',
+            location: {
+              "center": [
+                3.436099,
+                6.809385
+              ],
+              "zoom": 11.805394255186213,
+              "pitch": 33.499999999999986,
+              "bearing": 136.79999999999995
+            }
+        },
+        {
+            id: 'owerri-owerri',
+            title: 'Owerri, Owerri',
+            description: 'Lived here as a student',
+            location: {
+              "center": [
+                7.032668,
+                5.482655
+              ],
+              "zoom": 11.805394255186213,
+              "pitch": 33.499999999999986,
+              "bearing": 136.79999999999995
+            }
+        },
+        {
+            id: 'maryland-lagos',
+            title: 'Maryland, Lagos',
+            description: 'Fifth place I lived as a kid',
+            location: {
+              "center": [
+                3.369634,
+                6.573069
+              ],
+              "zoom": 11.805394255186213,
+              "pitch": 33.499999999999986,
+              "bearing": 136.79999999999995
+            }
+        },
+        {
+            id: 'gbagada-lagos',
+            title: 'Gbagada, Lagos',
+            description: 'Sixth place I lived as a kid',
+            location: {
+              "center": [
+                3.386104,
+                6.564899
               ],
               "zoom": 11.805394255186213,
               "pitch": 33.499999999999986,
